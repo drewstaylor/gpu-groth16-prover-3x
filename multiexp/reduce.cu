05b98e05b19dc2ec63fe9f5a3b33ef4333bd9a0e
Drew Taylor <drew@infiniteinternet.ca>
2019-07-24 16:00:19 -0400

    now that the math is working again :x see if we can get to 1024 threads per block

--- multiexp/reduce.cu
+++ multiexp/reduce.cu
@@ -9,7 +9,7 @@
 
 // C is the size of the precomputation
 // R is the number of points we're handling per thread
-template< typename EC, int C = 4, int RR = 16 >
+template< typename EC, int C = 4, int RR = 32 >
 //template< typename EC, int C = 4, int RR = 8 >
 __global__ void
 ec_multiexp_straus(var *out, const var *multiples_, const var *scalars_, size_t N)
@@ -144,8 +144,8 @@ ec_sum_all(var *X, const var *Y, size_t n)
 }
 
 //static constexpr size_t threads_per_block = 256;
-static constexpr size_t threads_per_block = 512;
-//static constexpr size_t threads_per_block = 2048;
+//static constexpr size_t threads_per_block = 512;
+static constexpr size_t threads_per_block = 1024;
 
 template< typename EC, int C, int R >
 void
