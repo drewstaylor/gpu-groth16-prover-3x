55dfeccc4ac5b8e0358a348b87ae1af77ab2d954
Drew Taylor <drew@infiniteinternet.ca>
2019-07-29 14:19:18 -0400

    attempt debug cuda make

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build
 pushd build
   cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
-  make -j12 main generate_parameters cuda_prover_piecewise
+  make -d -j12 main generate_parameters cuda_prover_piecewise
 popd
 mv build/libsnark/main .
 mv build/libsnark/generate_parameters .
