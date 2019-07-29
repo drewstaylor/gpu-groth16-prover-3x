4bfd3abc0b79707b9a4b37cd82749ff079188a8c
Drew Taylor <drew@infiniteinternet.ca>
2019-07-29 14:31:13 -0400

    cmake verbose = 1

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir build
 pushd build
-  cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF -VERBOSE=1 $EXTRA_CMAKE_ARGS_FOR_CI ..
-  make -j12 main generate_parameters cuda_prover_piecewise
+  cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
+  make -j12 -VERBOSE=1 main generate_parameters cuda_prover_piecewise
 popd
 mv build/libsnark/main .
 mv build/libsnark/generate_parameters .
